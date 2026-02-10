a3a1fe904df0170424b0e8818850f6c8b187e05e
ChatX <amannegi12947n@gmail.com>
2026-02-10 11:44:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+const dropdown = document.querySelector(".dropdown > a");
+const menu = document.querySelector(".dropdown-menu");
+
+dropdown.addEventListener("click", (e) => {
+  e.preventDefault();
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+});
